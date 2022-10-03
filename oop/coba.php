32639cc44bb7c7f64b45fe5f2b2664049efb893d
Fajar-NS <fajarns13@gmail.com>
2022-10-03 12:00:47 +0700

    class and objek

---
+++ oop/coba.php
@@ -0,0 +1,21 @@
+<?php
+
+class coba {
+    // public $a; //property
+
+    // //method
+    // public function b(){
+        
+    // }
+}
+ 
+$a = new coba();
+$b = new coba();
+$c = new coba();
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
+
+
+?>
---
+++ oop/produk.php
@@ -0,0 +1,13 @@
+<?php
+
+class produk{
+
+}
+
+$produk1 = new produk();
+$produk2 = new produk();
+
+var_dump($produk1);
+var_dump($produk2);
+
+?>

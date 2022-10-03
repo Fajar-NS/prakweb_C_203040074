2d9169a3462a165736f0b668a29fb46a3f3ecfcc
Fajar-NS <fajarns13@gmail.com>
2022-10-03 10:15:38 +0700

    ubah background

--- pw/index.php
+++ pw/index.php
@@ -10,9 +10,9 @@ $buku = query("SELECT * FROM buku");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Buku</title>
-    <link rel="stylesheet" href="css/style.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+    <link rel="stylesheet" href="css/style.css">
 
 </head>
 
